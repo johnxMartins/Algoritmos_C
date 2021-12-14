ac1f7504a313a9f7263fb2b8e65b187419c1576f
johnN <88109066+johnxMartins@users.noreply.github.com>
2021-12-13 22:33:41 -0300

    Add files via upload

--- ex1.c
+++ ex1.c
@@ -1,14 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 int main (){
-    int vet[3];
-    int i;
-    for (i = 0; i < 3; i++)
-    {
-        scanf("%d", &vet[i]);
+    int input[3],sorting[3], i, j, temp;
+    for(i = 0; < 3; i++){
+        scanf("%d", &input[i]);
+        sorting[i] = input[i];
     }
-    printf("%d\n", vet[0]);
-    printf("%d\n", vet[1]);
-    printf("%d\n", vet[2]);
+    for(i = 0; i < 2; i++){
+        for(j = i+1; j < 3; j++){
+            if(sorting[i] > sorting[j]){
+                temp = sorting[j];
+                sorting[j] = sorting[i];
+                sorting[i] = temp;
+            }
+        }
+    }
+    for (i = 0; i < 3; i++)
+        printf("%d\n",sorting[i]);
+    
     printf("\n");
     
+    for (i = 0; i < 3; i++)
+        printf("%d\n",input[i]);
+    
     return 0;
+}

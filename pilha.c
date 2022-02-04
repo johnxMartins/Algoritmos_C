9db12e7ffce19e68a36914679c93f05dc01bb6ed
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-03 21:05:56 -0300

    errr

--- pilha.c
+++ pilha.c
@@ -12,3 +12,39 @@ void TPilha_Inicia(TPilha *p)
 {
   p->Topo = -1;
 }
+
+int TPilha_Vazia(TPilha *p)
+{
+  if (p->Topo == -1)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int TPilha_Cheia(TPilha *p)
+{
+  if (p->Topo == MAXTAM - 1)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+void TPilha_Insere(TPilha *p, int x)
+{
+  if (TPilha_Cheia(p) == 1)
+  {
+    printf("ERRO: Pilha Cheia");
+  }
+  else
+  {
+    p->Topo++;
+  }
+}

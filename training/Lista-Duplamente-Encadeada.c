410b064985902dbc3b829b2d8895186a1cbf9fc3
johnxMartins <johnguilhermemartins@gmail.com>
2022-02-25 20:52:51 -0300

    inicializa lista

--- training/Lista-Duplamente-Encadeada.c
+++ training/Lista-Duplamente-Encadeada.c
@@ -10,3 +10,25 @@ typedef struct TipoNo
   struct TipoNo *proximo;
   struct TipoNo *anterior;
 } TNo;
+
+// Cria o inicio da lista
+typedef struct TipoLista
+{
+  TNo *inicio = NULL;
+  TNo *fim = NULL;
+} TLista;
+
+//------------------------------------------------------------------------------------
+// Funcao que define a Lista como vazia.
+void inicializaLista(TLista *pLista)
+{
+  pLista->inicio = NULL;
+  pLista->fim = NULL;
+}
+//------------------------------------------------------------------------------------
+// Funcao que insere um elemento do inicio da lista
+void inserirNoInicio(TLista *pLista)
+{
+  TNo *novoElemento;
+  novoElemento = new TNo;
+}
